0a3adec3ff55822a1f398408dbfb257da866d058
Jean-Dan10 <jean.daniel.laplante@gmail.com>
2023-09-27 14:17:34 -0400

    first commit

---
+++ UserController.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const UserController = {
+  async register(req, res) {
+    try {
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ username, password: hashedPassword });
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+
+      // Create and send a JWT token
+      const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  },
+};
+
+module.exports = UserController;
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth-routes');
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => {
+  console.log(`Authentication service is running on port ${PORT}`);
+});
