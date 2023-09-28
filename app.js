63a776bacdfbea7fa1bc5f47ebf8c8845870e91a
Jean-Dan10 <jean.daniel.laplante@gmail.com>
2023-09-28 16:28:33 -0400

    fix cors

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth-routes');
+const cors = require('cors'); 
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with the appropriate URL
+  credentials: true,
+
+};
+
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(bodyParser.json());
+
 
 // Routes
 app.use('/', authRoutes);
